bdf2c99c8846161d585888b2aed02435751b0c52
Igor Ryzhov <iryzhov@nfware.com>
2023-10-06 15:01:16 +0300

    mgmt, lib: differentiate DELETE and REMOVE operations
    Currently, there's a single operation type which doesn't return error
    if the object doesn't exists. To be compatible with NETCONF/RESTCONF,
    we should support differentiate between DELETE (fails when object
    doesn't exist) and REMOVE (doesn't fail if the object doesn't exist).
    
    Signed-off-by: Igor Ryzhov <iryzhov@nfware.com>

--- mgmtd/mgmt_txn.c
+++ mgmtd/mgmt_txn.c
@@ -912,7 +912,7 @@ static int mgmt_txn_create_config_batches(struct mgmt_txn_req *txn_req,
 			 */
 			if (chg->cb.operation == NB_CB_DESTROY)
 				batch->cfg_data[batch->num_cfg_data].req_type =
-					MGMTD__CFG_DATA_REQ_TYPE__DELETE_DATA;
+					MGMTD__CFG_DATA_REQ_TYPE__REMOVE_DATA;
 			else
 				batch->cfg_data[batch->num_cfg_data].req_type =
 					MGMTD__CFG_DATA_REQ_TYPE__SET_DATA;
@@ -2022,6 +2022,9 @@ int mgmt_txn_send_set_config_req(uint64_t txn_id, uint64_t req_id,
 
 		switch (cfg_req[indx]->req_type) {
 		case MGMTD__CFG_DATA_REQ_TYPE__DELETE_DATA:
+			cfg_chg->operation = NB_OP_DELETE;
+			break;
+		case MGMTD__CFG_DATA_REQ_TYPE__REMOVE_DATA:
 			cfg_chg->operation = NB_OP_DESTROY;
 			break;
 		case MGMTD__CFG_DATA_REQ_TYPE__SET_DATA:
